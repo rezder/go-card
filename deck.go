105048bd24eb23d26ca298289c2f0fff6bb9b9aa
Rene Olsen <wwwrene@runbox.com>
2016-03-26 17:32:16 +0100

    Switch to public variables.
    The switch was made to use save functionality.
    Added return functionality

--- deck.go
+++ deck.go
@@ -7,60 +7,163 @@ package deck
 
 import (
 	"errors"
+	"fmt"
 	"math/rand"
+	slice "rezder.com/slice/int"
 	"time"
 )
 
 type Deck struct {
-	perm []int
-	delt []bool
-	top  int
+	Perm     []int
+	Delt     []bool
+	Top      int
+	Returned []int
 }
 
 func New(i int) *Deck {
 	deck := Deck{
-		perm: rand.Perm(i),
-		delt: make([]bool, i), // should all be zero value
-		top:  0,
+		Perm: rand.Perm(i),
+		Delt: make([]bool, i), // should all be zero value
+		Top:  0,
 	}
 	return &deck
 }
-func (deck *Deck) Deal() (ix int, err error) {
-	if deck.top < len(deck.perm) {
-		ix = deck.perm[deck.top]
-		deck.delt[deck.top] = true
-		deck.top = deck.top + 1
+func (deck *Deck) Equal(other *Deck) (equal bool) {
+	if other == nil && deck == nil {
+		equal = true
+	} else if other != nil && deck != nil {
+		if deck == other {
+			equal = true
+		} else if other.Top == deck.Top {
+			if slice.Equal(other.Perm, deck.Perm) && slice.Equal(other.Returned, deck.Returned) {
+				equal = true
+				for i, v := range other.Delt {
+					if v != deck.Delt[i] {
+						equal = false
+						break
+					}
+				}
+			}
+		}
+	}
+	return equal
+}
+func (deck *Deck) Copy() (c *Deck) {
+	if deck != nil {
+		c = new(Deck)
+		c.Top = deck.Top
+		c.Perm = make([]int, len(deck.Perm))
+		copy(c.Perm, deck.Perm)
+		c.Delt = make([]bool, len(deck.Delt))
+		copy(c.Delt, deck.Delt)
+		c.Returned = make([]int, len(deck.Returned))
+		copy(c.Returned, deck.Returned)
+	}
+	return c
+}
+func (deck *Deck) String() (txt string) {
+	if deck != nil {
+		last := "nil"
+		if deck.Top != 0 {
+			last = fmt.Sprint(deck.Perm[deck.Top-1])
+		}
+		txt = fmt.Sprintf("Deck{Cards %v, Index: %v Last %v}", len(deck.Perm), deck.Top, last)
 	} else {
-		ix = -1
-		err = errors.New("Deck is empty")
+		txt = "Deck{nil}"
+	}
+	return txt
+}
+
+//Return the returned cards will added to the top and acces in reverse order,
+func (deck *Deck) Return(ret []int) {
+	if len(ret) != 0 {
+		deck.Returned = ret
+	}
+}
+
+func (deck *Deck) Deal() (ix int, err error) {
+	if len(deck.Returned) != 0 {
+		ix = deck.Returned[len(deck.Returned)-1]
+		deck.Returned = deck.Returned[:len(deck.Returned)-1]
+	} else {
+		if deck.Top < len(deck.Perm) {
+			ix = deck.Perm[deck.Top]
+			deck.Delt[deck.Top] = true
+			deck.Top = deck.Top + 1
+		} else {
+			ix = -1
+			err = errors.New("Deck is empty")
+		}
 	}
 	return ix, err
 }
 func (deck *Deck) DealCard(c int) (err error) {
-	var ix = -1
-	for i, v := range deck.perm {
-		if v == c {
-			ix = i
-			break
+	found := false
+	if len(deck.Returned) != 0 {
+		for i, v := range deck.Returned {
+			if v == c {
+				copy(deck.Returned[:i], deck.Returned[i+1:])
+				found = true
+			}
 		}
 	}
-	if ix != -1 {
-		if deck.delt[ix] {
-			err = errors.New("Card is allready delt")
+	if !found {
+		var ix = -1
+		for i, v := range deck.Perm {
+			if v == c {
+				ix = i
+				break
+			}
+		}
+		if ix != -1 {
+			if deck.Delt[ix] {
+				err = errors.New("Card is allready delt")
+			} else {
+				deck.Delt[ix] = true
+			}
+			deck.Delt[ix] = true
 		} else {
-			deck.delt[ix] = true
+			err = errors.New("Card do not exist")
 		}
-		deck.delt[ix] = true
-	} else {
-		err = errors.New("Card do not exist")
 	}
+
 	return err
 }
 func (deck *Deck) Scuffle() {
 	rand.Seed(time.Now().UnixNano())
-	deck.perm = rand.Perm(len(deck.delt))
-	for i := range deck.delt {
-		deck.delt[i] = false
+	deck.Perm = rand.Perm(len(deck.Delt))
+	for i := range deck.Delt {
+		deck.Delt[i] = false
 	}
-	deck.top = 0
+	deck.Top = 0
+	deck.Returned = nil
+}
+func (deck *Deck) Empty() (empty bool) {
+	if len(deck.Returned) == 0 {
+		empty = true
+		for _, delt := range deck.Delt {
+			if !delt {
+				empty = false
+				break
+			}
+		}
+	}
+	return empty
+}
+
+//Remaining returns the no delt cards in the order they would be delt in.
+func (deck *Deck) Remaining() (res []int) {
+	res = make([]int, 0, len(deck.Perm)-deck.Top+len(deck.Returned))
+	if len(deck.Returned) != 0 {
+		for _, v := range deck.Returned {
+			res = append(res, v)
+		}
+	}
+	for i := deck.Top; i < len(deck.Perm); i++ {
+		if !deck.Delt[i] {
+			res = append(res, deck.Perm[i])
+		}
+	}
+
+	return res
 }
--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,11 @@
 package deck
 
-import "testing"
+import (
+	"bytes"
+	"encoding/gob"
+	"os"
+	"testing"
+)
 
 type card struct {
 	color int
@@ -17,7 +22,17 @@ func TestDeck(t *testing.T) {
 	t.Logf("Scuffle")
 	deck.Scuffle()
 	logDeck(deck, d52, t)
-
+	deck.Scuffle()
+	ix, _ := deck.Deal()
+	t.Logf("Deal card %v,%v\n", co[d52[ix].color], d52[ix].value)
+	r := make([]int, 2)
+	r = append(r, ix)
+	ix, _ = deck.Deal()
+	t.Logf("Deal card %v,%v\n", co[d52[ix].color], d52[ix].value)
+	r = append(r, ix)
+	deck.Return(r)
+	logDeck(deck, d52, t)
+	t.Logf("Returned\n")
 }
 func logDeck(d *Deck, deck52 []card, t *testing.T) {
 	for i := 0; i < 52; i++ {
@@ -40,4 +55,76 @@ var co = [4]string{
 	"\u2666",
 	"\u2665",
 	"\u2660",
+}
+
+func TestSave(t *testing.T) {
+	fileName := "test/save.gob"
+	deck := New(10)
+	file, err := os.Create(fileName)
+	//f,err:=os.Open(fileName)
+
+	if err == nil {
+		err = Save(deck, file)
+		file.Close()
+		if err != nil {
+			t.Errorf("Save game file error. File :%v, Error: %v", fileName, err.Error())
+		} else {
+			file, err = os.Open(fileName)
+			saveDeck, err := Load(file)
+			if err != nil {
+				t.Errorf("Load game file error. File :%v, Error: %v", fileName, err.Error())
+			} else {
+				if !saveDeck.Equal(deck) {
+					t.Error("Save and load game not equal")
+				}
+			}
+		}
+	} else {
+		t.Errorf("Create file error. File :%v, Error: %v", fileName, err.Error())
+	}
+}
+func Save(game *Deck, file *os.File) (err error) {
+	encoder := gob.NewEncoder(file)
+	err = encoder.Encode(game)
+
+	return err
+}
+func Load(file *os.File) (deck *Deck, err error) {
+	decoder := gob.NewDecoder(file)
+	var d Deck
+	err = decoder.Decode(&d)
+	deck = &d
+	return deck, err
+}
+func TestDecoder(t *testing.T) {
+	deck := New(10)
+	b := new(bytes.Buffer)
+
+	e := gob.NewEncoder(b)
+
+	// Encoding the map
+	err := e.Encode(deck)
+	if err != nil {
+		t.Errorf("Error encoding")
+	}
+
+	var loadDeck Deck
+	d := gob.NewDecoder(b)
+
+	// Decoding the serialized data
+	err = d.Decode(&loadDeck)
+	if err != nil {
+		t.Errorf("Error decoding")
+	} else {
+		if !deck.Equal(&loadDeck) {
+			t.Logf("Deck :%v\nLoad :%v", deck, loadDeck)
+			t.Error("Save and load deck not equal")
+		}
+	}
+}
+func TestFormate(t *testing.T) {
+	value := *New(10)
+	value.Deal()
+	t.Logf("Value: %v,Pointer: %v", value, &value)
+	//	t.Error(" Fail")
 }
